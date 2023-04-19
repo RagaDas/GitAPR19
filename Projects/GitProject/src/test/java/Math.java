1ac9db09d10e91044bee7c3795501039c390073b
RagaDas <ragashanmukhadas@gmail.com>
2023-04-19 11:20:16 +0530

    Subtraction - First commit

--- Projects/GitProject/src/test/java/Math.java
+++ Projects/GitProject/src/test/java/Math.java
@@ -3,10 +3,16 @@ public class Math {
         int Sum = x+y;
         return Sum;
     }
+
+    public int Subtract(int x, int y){
+        int Diff = x+y;
+        return Diff;
+    }
     public static void main(String args[]){
         Math M = new Math();
         int Result = M.Add(5,3);
         System.out.println(Result);
-
+        int Result = M.Subtract(5,3);
+        System.out.println(Result);
     }
 }
